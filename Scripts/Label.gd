e1676719730b33d92e86fdfb6652291799b70796
ricky10aguilar <59854957+ricky10aguilar@users.noreply.github.com>
2020-02-10 19:47:52 -0500

    Meets execerise requirements

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -1,8 +1,10 @@
 extends Label
 
-
 func _ready():
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	pass
+	var mouse_pos = get_viewport().get_mouse_position()
+	print(mouse_pos.x)
+	text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -4,5 +4,12 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-func _physics_process(delta):
-	pass
+func _physics_process(_delta):
+	if Input.is_action_pressed("Left"):
+		position.x = position.x - 10
+	elif Input.is_action_pressed("Right"):
+		position.x = position.x + 10
+	if Input.is_action_pressed("Up"):
+		position.y = position.y - 10
+	elif Input.is_action_pressed("Down"):
+		position.y = position.y + 10
